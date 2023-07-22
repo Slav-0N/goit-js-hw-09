69fa79f49da06c6c9a11b44a846dd186a72ac864
Slav-0N <SOVILGO@gmail.com>
2023-07-22 21:40:39 +0300

    Завдання 1 виконано

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]')
+const body = document.querySelector('body');
+let changeBgColorId;
+const styleElement = document.querySelector('style');
+
+startButton.addEventListener('click', startClicking);
+function startClicking(event) {
+  console.log('You pushed Start button.');
+  changeBgColorId = setInterval(bodyStyleAdding, 1000);
+  startButton.setAttribute('disabled', '');
+}
+
+stopButton.addEventListener('click', stopClicking);
+function stopClicking(event) {
+  console.log('You pushed STOP button.');
+  clearInterval(changeBgColorId);
+  body.lastElementChild.innerHTML = '';
+  startButton.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+} 
+
+function bodyStyleAdding() {
+  if (document.querySelector('#style')) {
+    body.lastElementChild.remove();
+  };
+  body.insertAdjacentHTML('beforeend', `<style id='style'>body {background-color: ${getRandomHexColor()};}</style>`);
+};
+
+
+
+
+
+
 
