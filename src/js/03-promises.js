3b94cfa61b1869ff698cde64706198e26c85813f
Slav-0N <SOVILGO@gmail.com>
2023-07-27 23:26:48 +0300

    задача № 3 - це капець

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,59 @@
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onStart);
+
+
+
+function onStart(event) {
+  event.preventDefault();
+  const {  delay, step, amount } = formEl.elements;
+
+  for (let i = 0; i <= amount.value - 1; i += 1) {   
+    let pauseEl = Number(delay.value) + i * Number(step.value);
+
+    createPromise(i+1, pauseEl);  
+  }
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  
+  const promisEl = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({position,delay});
+      } else {
+        // Reject
+        reject({position,delay});
+      }
+    }, delay)
+  });
+
+
+  promisEl
+    .then(
+      ({position,delay}) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+        
+    .catch(
+     ({position,delay}) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+  
+};
+
+
+
+
